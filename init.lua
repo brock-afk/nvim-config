460d7f367155fd16d5965d507be32a7cc2d71f4e
brock-afk <brockbrumley95@gmail.com>
2024-12-07 12:19:50 -0600

    add lsp config

--- init.lua
+++ init.lua
@@ -12,14 +12,20 @@ vim.g.loaded_ruby_provider = 0
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 
+-- source files
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
+-- code action key maps
+vim.keymap.set('n', 'grn', vim.lsp.buf.rename)
+vim.keymap.set('n', 'gra', vim.lsp.buf.code_action)
+vim.keymap.set('n', 'grr', vim.lsp.buf.references)
+vim.keymap.set('i', '<C-s>', vim.lsp.buf.signature_help)
 
 vim.api.nvim_create_autocmd('TextYankPost', {
     desc = 'Highlight when yanking text',
     group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-    callback = function() 
+    callback = function()
 	vim.highlight.on_yank()
     end
 })
