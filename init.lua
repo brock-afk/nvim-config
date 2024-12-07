53dedc8502004a6307943388b1cfd31b18aade06
brock-afk <brockbrumley95@gmail.com>
2024-12-07 11:06:46 -0600

    initial commit

---
+++ init.lua
@@ -0,0 +1,24 @@
+require("config.lazy")
+
+local set = vim.opt
+
+set.number = true
+set.relativenumber = true
+set.shiftwidth = 4
+
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function() 
+	vim.highlight.on_yank()
+    end
+})
