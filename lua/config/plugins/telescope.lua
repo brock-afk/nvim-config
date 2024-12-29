e18de8e71c243153708d3d18ca0e3847b522a2fe
brock-afk <brockbrumley95@gmail.com>
2024-12-29 12:33:04 -0600

    add default theme for telescope

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -6,7 +6,19 @@ return {
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
     },
     config = function()
-        vim.keymap.set("n", "<space>f", require('telescope.builtin').find_files)
+        require('telescope').setup {
+            pickers = {
+                find_files = {
+                    theme = "ivy"
+                }
+            },
+            extensions = {
+                fzf = {}
+            }
+        }
+
+        vim.keymap.set("n", "<space>fh", require('telescope.builtin').help_tags)
+        vim.keymap.set("n", "<space>fd", require('telescope.builtin').find_files)
         vim.keymap.set("n", "<space>en", function()
             require('telescope.builtin').find_files {
                 cwd = vim.fn.stdpath("config")
