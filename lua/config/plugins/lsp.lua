8b5b5bbbc32148de5854e1ddf7337391f0a0ee12
brock <brock@correctionssoftware.com>
2024-12-31 19:36:00 -0600

    add open terminal keymap

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -19,7 +19,7 @@ return {
 
             require("lspconfig").lua_ls.setup { capabilities = capabilities }
             require("lspconfig").zls.setup { capabilities = capabilities }
-            require 'lspconfig'.rust_analyzer.setup {
+            require("lspconfig").rust_analyzer.setup {
                 capabilities = capabilities,
                 settings = {
                     ['rust-analyzer'] = {
