e83b13132711d53dd5b2b6cd289ba4dd39e8e057
brock <brock@correctionssoftware.com>
2025-01-12 08:25:59 -0600

    update cmp keymap

--- lua/config/plugins/cmp.lua
+++ lua/config/plugins/cmp.lua
@@ -3,7 +3,10 @@ return {
     dependencies = 'rafamadriz/friendly-snippets',
     version = '*',
     opts = {
-        keymap = { preset = 'default' },
+        keymap = { preset = 'super-tab' },
+        completion = {
+            accept = { auto_brackets = { enabled = false }, },
+        },
         appearance = {
             use_nvim_cmp_as_default = true,
             nerd_font_variant = 'mono'
