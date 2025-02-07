13571d62ce7865dbb69dc5c59c72575604888606
Mario Portillo <mario@correctionssoftware.com>
2025-02-07 15:20:07 -0600

    feat: add mason for automatic LSP installation

---
+++ lua/config/plugins/mason-lspconfig.lua
@@ -0,0 +1,10 @@
+return {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+        require("mason-lspconfig").setup({
+            ensure_installed = { "lua_ls", "pyright", "ruff", "zls", "intelephense", "rust_analyzer" },
+            automatic_installation = true,
+        })
+    end
+}
